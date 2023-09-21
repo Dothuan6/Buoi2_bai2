262c61c15aef036c84931cf1b05fd6273270af8f
Dothuan6 <thuann6222@gmail.com>
2023-09-21 19:55:00 +0700

    hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,5 @@
 public class hello {
         public static void main(String[] args) {
-            System.out.println("Hello World!"); // Display the string.
+            System.out.print("Hello World!"); // Display the string.
         }
 }
